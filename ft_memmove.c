5c8cc7ece6c6d6a28fda933eed93c097b44f9d2b
Rodrigo Miguel Morais Malta Da Silva <108375741+rodas96@users.noreply.github.com>
2022-10-27 12:19:44 +0100

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -14,28 +14,21 @@
 
 void *ft_memmove(void *dest, const void *src, size_t n)
 {
-	char	*d;
-	char	*s;
-	size_t len;
-	
+	unsigned char *d;
+	unsigned const char *s;
+
 	d = dest;
 	s = src;
-	len = 0;
-	if (s < d)
+	if (dest < src)
 	{
-		while (n > 0)
-		{
-			n--;
-			d[n] = s[n];
-		}
+		ft_memcpy(dest, src, n);
+
 	}
 	else
-	{
-		while (len < n)
+		while(n > 0)
 		{
-			d[len] = s[len];
-			len++;
+			*d++ = *s++;
+			n--;
 		}
-	}
-	return (dest);
+	return(dest);
 }
